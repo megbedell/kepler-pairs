242d1247ebb1a29a64f583b5a974f115a028ce11
megbedell <megbedell@gmail.com>
2018-05-21 14:01:02 -0400

    bug fix, separate MPI script

--- code/find_pairs_mpi.py
+++ code/find_pairs_mpi.py
@@ -2,12 +2,10 @@ import numpy as np
 from astropy.table import Table, unique
 from astropy import units as u
 from astropy.io import fits
-import matplotlib.pyplot as plt
 from tqdm import tqdm
 import pandas as pd
 from schwimmbad import SerialPool, MultiPool, MPIPool
 import h5py
-from plot_tools import error_ellipse
 import time
 
 min_columns = ['source_id', 'ra', 'dec', 'parallax', 'pmra', 'pmdec', 'parallax_error',
@@ -35,13 +33,6 @@ def construct_table(gaia_table_file, kepler_table_file=None, minimal=True):
 def read_from_fits(filename):
     hdul = fits.open(filename)
     return hdul[0].data
-
-def save_as_fits(filename, data):
-    print("saving as {0}...".format(filename))
-    hdu = fits.PrimaryHDU(data)
-    hdul = fits.HDUList([hdu])
-    hdul.writeto(filename, overwrite=True)
-    hdul.close()
 
 def make_x(star):
     """
@@ -112,16 +103,6 @@ def calc_chisqs_for_row(i,row):
         row_of_chisqs[row_mask] = matches.apply(calc_chisq_for_pair, args=(primary,), axis=1)
     return row_of_chisqs
 
-def calc_chisqs_for_table(table, pairs, save=False, save_every=1e6, save_name='chisqs.fits'):
-    chisqs = np.zeros_like(pairs) - 1.
-    for i,row in tqdm(enumerate(pairs)):
-        chisqs[i] = calc_chisqs_for_row(i,row)
-        if save and (i % save_every == 1):
-            save_as_fits(save_name, chisqs)
-    if save:
-        save_as_fits(save_name, chisqs)
-    return chisqs
-
 def worker(data):
     """
     Wrapper function for parallelization
@@ -137,54 +118,30 @@ def callback(results):
     i, row_of_chisqs = results
     with h5py.File('chisqs.hdf5', 'r+') as f:
         f['chisqs'][i] = row_of_chisqs
-
-def read_match_attr(table, ind1, ind2, attr):
-    return table.iloc[ind1][attr], table.iloc[ind2][attr]
-
-def plot_xs(table, i, sigma=1):
-    fs = 12
-    star1 = table.iloc[pairs_ind1s[i]]
-    star2 = table.iloc[pairs_ind2s[i]]
-    x1 = make_x(star1)
-    cov1 = make_cov(star1)
-    x2 = make_x(star2)
-    cov2 = make_cov(star2)
-    fig = plt.figure(figsize=(12,4))
-    ax1 = fig.add_subplot(131)
-    error_ellipse(ax1, x1[0], x1[1], cov1[:2,:2], ec='red', sigma=sigma)
-    error_ellipse(ax1, x2[0], x2[1], cov2[:2,:2], ec='blue', sigma=sigma)
-    ax1.set_xlim([min([x1[0], x2[0]]) - 5., max([x1[0], x2[0]]) + 5.])
-    ax1.set_ylim([min([x1[1], x2[1]]) - 5., max([x1[1], x2[1]]) + 5.])
-    ax1.set_xlabel('Parallax (mas)', fontsize=fs)
-    ax1.set_ylabel('PM RA (mas yr$^{-1}$)', fontsize=fs)
-
-    ax2 = fig.add_subplot(133)
-    error_ellipse(ax2, x1[1], x1[2], cov1[1:,1:], ec='red', sigma=sigma)
-    error_ellipse(ax2, x2[1], x2[2], cov2[1:,1:], ec='blue', sigma=sigma)
-    ax2.set_xlim([min([x1[1], x2[1]]) - 5., max([x1[1], x2[1]]) + 5.])
-    ax2.set_ylim([min([x1[2], x2[2]]) - 5., max([x1[2], x2[2]]) + 5.])
-    ax2.set_xlabel('PM RA (mas yr$^{-1}$)', fontsize=fs)
-    ax2.set_ylabel('PM Dec (mas yr$^{-1}$)', fontsize=fs)
-
-    ax3 = fig.add_subplot(132)
-    c1 = np.delete(np.delete(cov1, 1, axis=0), 1, axis=1)
-    c2 = np.delete(np.delete(cov2, 1, axis=0), 1, axis=1)
-    error_ellipse(ax3, x1[0], x1[2], c1, ec='red', sigma=sigma)
-    error_ellipse(ax3, x2[0], x2[2], c2, ec='blue', sigma=sigma)
-    ax3.set_xlim([min([x1[0], x2[0]]) - 5., max([x1[0], x2[0]]) + 5.])
-    ax3.set_ylim([min([x1[2], x2[2]]) - 5., max([x1[2], x2[2]]) + 5.])
-    ax3.set_xlabel('Parallax (mas)', fontsize=fs)
-    ax3.set_ylabel('PM Dec (mas yr$^{-1}$)', fontsize=fs)
-
-    fig.subplots_adjust(wspace = 0.5)
-    fig.text(0.5, 0.95, 'match #{0}'.format(i), horizontalalignment='center',
-             transform=ax3.transAxes, fontsize=fs+2)
+        
+def main(pool):
+    """
+    Main function for MPIPool
+    see example: https://schwimmbad.readthedocs.io/en/latest/examples/index.html#using-mpipool
+    """
+    # make the output file
+    with h5py.File('chisqs.hdf5', 'w') as f:
+        dset = f.create_dataset('chisqs', data=np.zeros_like(pairs) - 1)
+     
+    # construct tasks list   
+    tasks_start = time.time()
+    tasks = list(enumerate(pairs))
+    tasks_end = time.time()
+    print("constructing tasks took {0} s".format(tasks_end - tasks_start))
+    
+    # run
+    results = pool.map(worker, tasks, callback=callback)
+    pool.close()
 
 if __name__ == '__main__':
     print("loading data...")
     start = time.time()
     gaia_table_file = '../data/gaia-kepler-dustin.fits'
-    kepler_table_file = '../data/kepler_dr2_1arcsec.fits'
     table = construct_table(gaia_table_file, minimal=True) # table is a global variable
     print("loading data table took {0} s".format(time.time() - start))
 
@@ -195,44 +152,13 @@ if __name__ == '__main__':
     print("loading pairs array took {0} s".format(time.time() - pairs_start))
 
     print("calculating chisquared...")
-    with h5py.File('chisqs.hdf5', 'w') as f:
-        dset = f.create_dataset('chisqs', data=np.zeros_like(pairs) - 1)
+    pool_start = time.time()
+    pool = MPIPool()
+    if not pool.is_master():
+        pool.wait()
+        sys.exit(0)
 
-    tasks_start = time.time()
-    tasks = list(enumerate(pairs))
-    tasks_end = time.time()
-    print("constructing tasks took {0} s".format(tasks_end - tasks_start))    
-
-    pool = MultiPool()
-    map_start = time.time()
-    results = pool.map(worker, tasks, callback=callback)
-    map_end = time.time()
-    print("mapping took {0} s".format(map_end - map_start))
-    pool.close()
-
-
-    if True: # basic diagnostics
-        with h5py.File('chisqs.hdf5', 'r+') as f:
-            chisqs = np.copy(f['chisqs'])
-        
-        plt.hist(chisqs[(chisqs > 0.) & (chisqs < 50.)], bins=500)
-        plt.xlabel('$\chi^2$', fontsize=16)
-        plt.ylabel('# Pairs', fontsize=16)
-        plt.yscale('log')
-        plt.savefig('chisq_keplerpairs.png')
-
-        matches_mask = (chisqs > 0) & (chisqs < 5)
-        print("{0} matches found with chisq < 5".format(np.sum(matches_mask)))
-        len_inds, len_matches = np.shape(pairs)
-        pairs_inds = np.array([np.arange(len_inds),]*len_matches).transpose()
-        pairs_ind1s = pairs_inds[matches_mask]
-        pairs_ind2s = pairs[matches_mask]
-
-        i = np.random.randint(0, len(pairs_ind1s))
-        print("match {0}: source_ids {1}".format(i,
-                    read_match_attr(table, pairs_ind1s[i], pairs_ind2s[i], 'source_id')))
-        print("saved chisquared = {0:.5f}".format(chisqs[pairs_ind1s[i]][np.where(pairs[pairs_ind1s[i]]
-                                                                            == pairs_ind2s[i])[0][0]]))
-        plot_xs(i, sigma=3)
-
+    main(pool)
+    pool_end = time.time()
+    print("pool execution took {0} s".format(pool_end - pool_start))
     print("total execution took {0} s".format(time.time() - start))
